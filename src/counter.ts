06b53999bcf5a567eb7a5750dab84c81725f4cd7
lsandoval9 <lsandoval9_dev@outlook.com>
2025-03-30 13:46:24 -0400

    add documentation

--- src/counter.ts
+++ src/counter.ts
@@ -2,18 +2,26 @@ export function setupCounter(element: HTMLButtonElement) {
   const orderBooks = () => {
     const container = document.getElementById('bookContainer')
 
+    // seleccionamos todos los libros, los cuales estan dentro de un div con la clase card
     const books = container?.querySelectorAll('.card')
 
+    // si hay al menos un libro, los ordenamos por el nombre del libro
     if (books) {
       const booksArray = Array.from(books)
+
+      // ordenamos los libros por el nombre del libro
       booksArray.sort((a, b) => {
         const titleA = a.querySelector('.card-title')?.textContent || ''
         const titleB = b.querySelector('.card-title')?.textContent || ''
         return titleA.localeCompare(titleB)
       })
+    
+      // limpiamos el contenedor de libros y añadimos los libros ya ordenados
       container.innerHTML = ''
       booksArray.forEach((book) => container?.appendChild(book))
     }
   }
+
+  // añadimos el evento al boton de ordenar libros
   element.addEventListener('click', orderBooks)
 }
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import './style.css'
 import { setupCounter } from './counter.ts'
 import fantasyBooks from '../json/fantasy-books.json'
 
-// Function to create a card for each book
-function createBookCard(book) {
-  const cardDiv = document.createElement('div');
-  cardDiv.classList.add('card', 'mb-3');
 
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body', 'text-center', 'd-flex', 'justify-content-between');
+function createBookCard(book: any) {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card', 'mb-3');
 
+    const cardBody = document.createElement('div');
+    // Usamos flex para que la imagen y el texto estén uno al lado del otro
+    cardBody.classList.add('card-body', 'text-center', 'd-flex', 'justify-content-between');
+
+
+    // dividimos el cardBody en dos columnas
     let firstColumn = document.createElement('div');
     firstColumn.classList.add('col-6');
     cardBody.appendChild(firstColumn);
@@ -18,49 +21,44 @@ function createBookCard(book) {
     secondColumn.classList.add('col-6');
     cardBody.appendChild(secondColumn);
 
-  const title = document.createElement('h5');
-  title.classList.add('card-title');
-  title.textContent = book.title;
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.textContent = book.title;
 
-  const author = document.createElement('p');
-  author.classList.add('card-text');
-  author.textContent = `Author: ${book.authors[0].name}`;
+    const author = document.createElement('p');
+    author.classList.add('card-text');
+    author.textContent = `Author: ${book.authors[0].name}`;
 
+    // revisamos si el libro tiene imagen y la agregamos si es el caso
+    const imageSrc = book.formats?.['image/jpeg'];
 
-  const imageSrc = book.formats?.['image/jpeg'];
+    let image = null;
 
-  let image = null;
+    if (imageSrc) {
+        image = document.createElement('img');
+        image.classList.add('card-img-top');
+        image.src = imageSrc;
+        image.alt = book.title;
+    }
 
-  if (imageSrc) {
-    image = document.createElement('img');
-    image.classList.add('card-img-top');
-    image.src = imageSrc;
-    image.alt = book.title;
-  }
-  
-    
-
-  // Add other book properties as needed (e.g., subjects, formats, etc.)
-
-  secondColumn.appendChild(title);
-  secondColumn.appendChild(author);
-  cardDiv.appendChild(cardBody);
-
-  if (image) {
-    firstColumn.appendChild(image);
-  }
+    secondColumn.appendChild(title);
+    secondColumn.appendChild(author);
+    cardDiv.appendChild(cardBody);
+    // añadimos la imagen a la primera columna si existe
+    if (image) {
+        firstColumn.appendChild(image);
+    }
 
 
-  return cardDiv;
+    return cardDiv;
 }
 
-// Get the container where cards will be added
 const container = document.getElementById('bookContainer');
 
-// Loop through the books and create cards dynamically
+
 fantasyBooks.results.forEach((book) => {
-  const bookCard = createBookCard(book);
-  container.appendChild(bookCard);
+    const bookCard = createBookCard(book);
+    container?.appendChild(bookCard);
 });
 
 
